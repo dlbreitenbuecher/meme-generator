fbf1422e6d77a8ccd740e02766d972aeef3a7229
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-07-03 14:32:54 -0700

    delete meme functionality added

--- app.js
+++ app.js
@@ -8,41 +8,18 @@ window.onload = function() {
         event.preventDefault();
 
         // Identifies Meme Container
-        const memeShowcase = this.document.getElementById('memeShowcase');
-
-
-
-
-        // Attempt without the image container
-        // const image = document.createElement('img');
-        // image.classList.add('memeImage');
-        // const src = document.getElementById('imageURL').value;
-        // image.setAttribute('src', src);
-        // memeShowcase.appendChild(image);
-
+        var memeShowcase = this.document.getElementById('memeShowcase');
 
         // Create Image Container
-        const imgFlexContainer = document.createElement('div');
+        var imgFlexContainer = document.createElement('div');
         imgFlexContainer.classList.add('imgFlexContainer');
         memeShowcase.appendChild(imgFlexContainer);
         // Display Image from URL
-        const image = document.createElement('img');
+        var image = document.createElement('img');
         image.classList.add('memeImage');
-        const src = document.getElementById('imageURL').value;
+        var src = document.getElementById('imageURL').value;
         image.setAttribute('src', src);
         imgFlexContainer.appendChild(image);
-
-
-        // let img = new this.Image();
-        // img.src = this.document.getElementById('imageURL');
-        // memeShowcase.appendChild(img);
-
-        // var img = document.createElement('img');
-        // img.classList.add('memeImage');
-        // var src = document.getElementById('imageURL').value;
-        // img.src = src;
-        // memeShowcase.appendChild(img);
-
 
 
         // Display Text on Image
@@ -64,9 +41,54 @@ window.onload = function() {
         imgFlexContainer.appendChild(lowerText);
 
 
+
+        // Hover State to Delete Image
+        imgFlexContainer.onmouseenter=function mouseOver(){
+            imgFlexContainer.classList.add('deleteImage');
+            let deleteButton = document.createElement('button');
+            deleteButton.classList.add('deleteButton');
+            deleteButton.innerText = 'X';
+            imgFlexContainer.setAttribute('id', 'test');
+            imgFlexContainer.appendChild(deleteButton);
+
+            // deleteButton.onclick=function deleteImage() {
+            //     memeShowcase.removeChild(imgFlexContainer);
+            // }
+
+
+            deleteButton.addEventListener('click', function(event) {
+                console.log(event.target.parentNode.parentNode);
+                event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+            });
+        };
+        // Removing Hover State to Delete Image
+        imgFlexContainer.onmouseleave = function mouseLeave(){
+            imgFlexContainer.classList.remove('deleteImage');
+            imgFlexContainer.removeChild(document.querySelector('button'));
+            imgFlexContainer.removeAttribute('id', 'test');
+        }
+
+
     });
+    
+    // function mouseOver(event) {
+    //     event.target.setAttribute('id', 'deleteImage');
+    // }
 
+    // let test = document.querySelectorAll('img');
+    // for (let i=0; i<test.length; i++) {
+    //     test[i].addEventListener('mouseover', mouseOver(event));
+    // }
 
+    // var imageContainer = document.getElementsByClassName('imgFlexContainer');
+    // for(var i=0; i<imageContainer.length; i++) {
+    //     document.querySelector(imageContainer[i] '.lowerText').addEventListener('mouseover', mouseOverDelete);
+    // }
+
+    // image.addEventListener('mouseover', () => {
+    //     let lowerText = document.getElementsByClassName('lowerText');
+    //     lowerText.innerText = 'Hover';
+    // })
 
 
 }
